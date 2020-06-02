8133c2b6dff13fd30c10790670513c94ff20a16d
karanitty <karanitty@gmail.com>
2020-06-02 14:15:09 +0530

    ok bei

--- Hello.java
+++ Hello.java
@@ -1,7 +1,19 @@
-class Hello
+class Super
+{
+	void display()
+	{
+		System.out.println("Good Morning");
+	}
+}
+
+
+
+class Hello extends Super
 {
 	public static void main (String args[])
 	{
 		System.out.println("Hello World");
+		Hello a = new Hello();
+		a.display();
 	}
 }
