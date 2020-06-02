12aa04798c7c289508509ad76e923e301d8a31f6
karanitty <karanitty@gmail.com>
2020-06-02 15:11:01 +0530

    last edit

--- Morning.java
+++ Morning.java
@@ -4,9 +4,20 @@ class Newthread extends Thread
 	{
 		for(int i=0; i<5; i++)
 		{
-			System.out.println("Thread = " +i);
+			System.out.println("Thread i = " +i);
 		}
 		System.out.println("Exiting from thread");
+	}
+}
+
+class Alright implements Runnable
+{
+	public void run()
+	{
+		for(int j=5; j>0; j--)
+		{
+			System.out.println("Thread j = " +j);
+		} 
 	}
 }
 
@@ -16,5 +27,8 @@ class Morning
 	{
 		Newthread a = new Newthread();
 		a.start();
+		Alright b = new Alright();
+		Thread t = new Thread(b);
+		t.start();
 	}
 }
